eeac10429fa8a2bd984a458e1d00cf68e3ecff8d
perillamint <perillamint@silicon.moe>
2024-08-22 00:19:55 +0900

    Implement read_config_file()

--- caplan/src/types/config.rs
+++ caplan/src/types/config.rs
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 use serde::{Deserialize, Serialize};
+use tokio::fs;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Database {
@@ -17,4 +18,21 @@ pub struct ActivityPub {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Config {
     pub activity_pub: ActivityPub,
+}
+
+pub(crate) fn parse_toml(tomlstr: &str) -> Config {
+    let cfg: Config = toml::from_str(tomlstr).expect("Invalid config file");
+
+    // TODO: Sanitize the value?
+
+    cfg
+}
+
+pub(crate) async fn read_config_file(cfgpath: &str) -> Config {
+    let cfg_str = fs::read_to_string(cfgpath)
+        .await
+        .expect("failed to read config file");
+
+    // TODO: Postprocess parsed config file
+    parse_toml(&cfg_str)
 }
